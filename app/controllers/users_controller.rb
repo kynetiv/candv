0b4a35d21058de1669212c1ab02f49bbab0ea09f
kynetiv <miles@kynetiv.com>
2013-04-07 00:27:15 -0700

    Add Show, Edit and Delete Functionality - Controllers and Views - Basic III

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,21 +4,46 @@ class UsersController < ApplicationController
 
   end
 
+  def show
+  	@user = User.find(params[:id])
+
+  end
+
   def new
 
+  end
 
+  def edit
+  	@user = User.find(params[:id])
   end
 
   def create
   	@new_user = User.new(params[:new_user])
 
-  	if @new_user.save
-  		flash[:success] = "Success!"
-  		redirect_to users_path
-  	else 
-  		@new_user.errors
-  		render 'new'
+  		if @new_user.save
+  			flash[:success] = "Success: New User Created!"
+  			redirect_to users_path
+  	
+  		else render 'new'
+		end
 	end
 
-end
+	def update
+		@user = User.find(params[:id])
+
+		if @user.update_attributes(params[:update])
+			flash[:success] = "User Profile Updated!"
+			redirect_to @user
+
+		else render 'edit'
+		end
+	end
+
+	def destroy 
+		@user = User.find(params[:id])
+		@user.destroy
+
+		flash[:danger] = "Account Deleted!"
+		redirect_to users_url
+	end
 end
